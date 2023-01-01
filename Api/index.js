1b0e70d8b896638d46670bdecf11e4add2708a8b
JosePeresini <joseignacioperesini@gmail.com>
2023-01-01 20:07:42 -0300

    bad folder

--- Api/index.js
+++
@@ -1,6 +0,0 @@
-
-const moongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://eurocase999:Programa2022@cluster0.bxzfuvg.mongodb.net/myApp?retryWrites=true&w=majority')
-
-
--- README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript-Sin-Fronteras
-Curso de Udemy de Nicolás Schrümann Lindemann
